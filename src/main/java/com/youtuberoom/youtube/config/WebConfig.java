9b38d551db3ea1b80bc661b64810f00c2a21c84a
Aayush <aayushmaheshwari2103@gmail.com>
2026-03-25 02:17:12 +0530

    Fix CORS to allow live frontend

--- src/main/java/com/youtuberoom/youtube/config/WebConfig.java
+++ src/main/java/com/youtuberoom/youtube/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173",
-                        "https://your-frontend.onrender.com")
+                        "https://https://youtube-room-frontend.onrender.com")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
